0b1716cdf5b1735679ddc7437d8f2d47fda81cb1
Sergio Betancor <serbetancor@gmail.com>
2025-01-31 11:25:56 +0000

    feat: improve songs, albums, artists & genres display

--- itunes-wrapped-front/src/views/ArtistsView.vue
+++ itunes-wrapped-front/src/views/ArtistsView.vue
@@ -5,7 +5,9 @@ import { ref, computed } from 'vue'
 import type { Artist } from '@/models/itunes'
 
 import ArrowIcon from '@/assets/arrow.svg'
+import HyphenIcon from '@/assets/hyphen.svg'
 import NoImageIcon from '@/assets/no-image.svg'
+import TopArtistCard from '@/components/TopArtistCard.vue'
 import { formatMilliseconds } from '@/utils/itunes'
 
 const artists = ref<Artist[]>(library.data)
@@ -19,15 +21,24 @@ const shownArtists = computed(() => artists.value.slice(0, topCount.value))
     <input type="range" v-model="topCount" min="1" max="400" class="mb-4 w-64" />
     <span>Top {{ topCount }} artist{{ topCount > 1 ? 's' : '' }}</span>
 
-    <div class="w-1/2 px-4">
+    <div class="w-10/12 px-4">
       <h3 class="mb-2 text-lg font-bold">Ranking</h3>
+      <div class="flex gap-2">
+        <TopArtistCard
+          v-for="(artist, index) in shownArtists.slice(0, 5)"
+          :key="artist.id"
+          :artist
+          :index
+        />
+      </div>
+
       <ul class="divide-y">
         <li
-          v-for="(artist, index) in shownArtists"
+          v-for="(artist, index) in shownArtists.slice(5)"
           :key="artist.id"
           class="grid min-h-12 grid-cols-[auto_auto_1fr_auto_auto] items-center gap-3 p-2 py-1 odd:bg-pink/10 even:bg-white"
         >
-          <span class="w-4 font-semibold">{{ index + 1 }}</span>
+          <span class="w-4 font-semibold">{{ index + 6 }}</span>
           <img v-if="artist.image" class="w-10 rounded-full" :src="artist.image" />
           <NoImageIcon v-else class="w-10 rounded-full border p-2" />
           <span>{{ artist.name }}</span>
@@ -39,7 +50,7 @@ const shownArtists = computed(() => artists.value.slice(0, topCount.value))
               'text-green': artist.positionsGained > 0,
             }"
           >
-            <span v-if="artist.positionsGained === 0" class="w-4 text-center">-</span>
+            <HyphenIcon v-if="artist.positionsGained === 0" class="w-4" />
             <ArrowIcon v-else class="w-4" :class="{ 'rotate-180': artist.positionsGained < 0 }" />
             <span class="w-5 text-right">{{ artist.positionsGained }}</span>
           </div>
