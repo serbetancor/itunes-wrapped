736b0a6bd6de42411e029d48a1f590c9e74d0171
Sergio Betancor <serbetancor@gmail.com>
2025-01-31 11:25:56 +0000

    feat: improve songs, albums, artists & genres display

--- itunes-wrapped-front/src/App.vue
+++ itunes-wrapped-front/src/App.vue
@@ -5,12 +5,12 @@ const topics = ['songs', 'albums', 'artists', 'genres']
 </script>
 
 <template>
-  <div class="flex h-full w-full flex-col bg-white">
+  <div class="flex min-h-screen w-full flex-col bg-zinc-700 text-white">
     <div class="mt-12 flex h-fit w-full justify-center gap-4">
       <button
         v-for="topic in topics"
         :key="topic"
-        class="text-gray border-b border-blue p-2 text-lg transition-all duration-300 ease-in-out hover:scale-110 hover:text-blue"
+        class="text-gray border-blue hover:text-blue border-b p-2 text-lg transition-all duration-300 ease-in-out hover:scale-110 hover:cursor-pointer"
         @click="getRouter().push({ name: topic })"
       >
         <span>{{ topic }}</span>
